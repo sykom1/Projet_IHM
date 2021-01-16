e2649cd078a6bbcb6c4d5230b2eb0d7e0b56052a
Anthony <anthony_pro1998@outlook.fr>
2021-01-16 16:46:45 +0100

    update english.ts

--- english.ts
+++ english.ts
@@ -19,39 +19,59 @@
         <translation type="unfinished">Retouch</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="55"/>
+        <location filename="mainwindowmenu.ui" line="48"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="52"/>
+        <source>Langue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="69"/>
         <source>Ouvrir une image</source>
         <translation type="unfinished">Open an image</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="63"/>
+        <location filename="mainwindowmenu.ui" line="77"/>
         <source>Sauvegarder sous</source>
         <translation type="unfinished">Save as</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="68"/>
+        <location filename="mainwindowmenu.ui" line="82"/>
         <source>Quitter</source>
         <translation type="unfinished">Quit</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="76"/>
+        <location filename="mainwindowmenu.ui" line="90"/>
         <source>Fermer l&apos;image</source>
         <translation type="unfinished">Close Image</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="81"/>
+        <location filename="mainwindowmenu.ui" line="98"/>
         <source>Filtre</source>
         <translation type="unfinished">Filter</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="86"/>
+        <location filename="mainwindowmenu.ui" line="103"/>
         <source>Redimensionnement</source>
         <translation type="unfinished">Resize</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="91"/>
+        <location filename="mainwindowmenu.ui" line="108"/>
         <source>Rogner</source>
         <translation type="unfinished">Crop</translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="113"/>
+        <source>Français</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="118"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindowmenu.cpp" line="19"/>
