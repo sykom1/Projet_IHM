43e5ee086ca5addca9dcd89d5225dbd2569055af
sykom1 <saber.tnifas@etu.univ-amu.fr>
2021-01-16 16:29:19 +0100

    Ajout de la langue anglaise

--- Projet_IHM.pro
+++ Projet_IHM.pro
@@ -17,11 +17,13 @@ HEADERS += \
 
 FORMS += \
     mainwindowmenu.ui
-TRANSLATIONS += french.ts \
-english.ts
+TRANSLATIONS += english.ts
 
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    translations.qrc
--- english.ts
+++ english.ts
@@ -1,67 +1,67 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>mainWindowMenu</name>
     <message>
         <location filename="mainwindowmenu.ui" line="14"/>
         <source>mainWindowMenu</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Main Menu</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="28"/>
         <source>Fichier</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">File</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="40"/>
         <source>Retouche</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Retouch</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="55"/>
         <source>Ouvrir une image</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Open an image</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="63"/>
         <source>Sauvegarder sous</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Save as</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="68"/>
         <source>Quitter</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Quit</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="76"/>
         <source>Fermer l&apos;image</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Close Image</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="81"/>
         <source>Filtre</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Filter</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="86"/>
         <source>Redimensionnement</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Resize</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.ui" line="91"/>
         <source>Rogner</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Crop</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.cpp" line="19"/>
         <source>Sélectionnez l&apos;image</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Select image</translation>
     </message>
     <message>
         <location filename="mainwindowmenu.cpp" line="20"/>
         <source>Fichier Image (*.png *.jpg *.bmp)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Image File</translation>
     </message>
 </context>
 </TS>
