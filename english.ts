dbfd0f94a8aec6edccd63d883546aa98d88fd3db
Anthony <anthony_pro1998@outlook.fr>
2021-01-16 11:30:43 +0100

    add english and french file for multi language

---
+++ english.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>mainWindowMenu</name>
+    <message>
+        <location filename="mainwindowmenu.ui" line="14"/>
+        <source>mainWindowMenu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="28"/>
+        <source>Fichier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="40"/>
+        <source>Retouche</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="55"/>
+        <source>Ouvrir une image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="63"/>
+        <source>Sauvegarder sous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="68"/>
+        <source>Quitter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="76"/>
+        <source>Fermer l&apos;image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="81"/>
+        <source>Filtre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="86"/>
+        <source>Redimensionnement</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="91"/>
+        <source>Rogner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.cpp" line="19"/>
+        <source>Sélectionnez l&apos;image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.cpp" line="20"/>
+        <source>Fichier Image (*.png *.jpg *.bmp)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
