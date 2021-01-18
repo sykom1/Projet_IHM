c8c97d49bf3f7fbd434ada80e5f44e4e82d87885
Anthony <anthony_pro1998@outlook.fr>
2021-01-18 15:33:51 +0100

    update english.ts

--- english.ts
+++ english.ts
@@ -19,47 +19,107 @@
         <translation type="unfinished">Retouch</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="55"/>
+        <location filename="mainwindowmenu.ui" line="60"/>
+        <source>Miroir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="78"/>
+        <source>Selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="93"/>
         <source>Ouvrir une image</source>
         <translation type="unfinished">Open an image</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="63"/>
+        <location filename="mainwindowmenu.ui" line="101"/>
         <source>Sauvegarder sous</source>
         <translation type="unfinished">Save as</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="68"/>
+        <location filename="mainwindowmenu.ui" line="106"/>
         <source>Quitter</source>
         <translation type="unfinished">Quit</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="76"/>
+        <location filename="mainwindowmenu.ui" line="114"/>
         <source>Fermer l&apos;image</source>
         <translation type="unfinished">Close Image</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="84"/>
+        <location filename="mainwindowmenu.ui" line="137"/>
+        <source>Rectangle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="145"/>
+        <source>Cercle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="150"/>
+        <source>1980 par 1024</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="155"/>
+        <source>1024 par 768</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="160"/>
+        <source>1680 par 1050</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="165"/>
+        <source>1600 par 900</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="170"/>
+        <source>Inverser Pixels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="175"/>
+        <source>Horizontal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="180"/>
+        <source>Vertical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="185"/>
+        <source>Supprimer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.ui" line="56"/>
         <source>Filtre</source>
         <translation type="unfinished">Filter</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="89"/>
+        <location filename="mainwindowmenu.ui" line="44"/>
         <source>Redimensionnement</source>
         <translation type="unfinished">Resize</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="94"/>
+        <location filename="mainwindowmenu.ui" line="119"/>
         <source>Rogner</source>
         <translation type="unfinished">Crop</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="99"/>
+        <location filename="mainwindowmenu.ui" line="124"/>
         <source>Français</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.ui" line="104"/>
+        <location filename="mainwindowmenu.ui" line="129"/>
         <source>English</source>
         <translation type="unfinished"></translation>
     </message>
@@ -69,14 +129,20 @@
         <translation type="unfinished">Image Touch</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.cpp" line="19"/>
+        <location filename="mainwindowmenu.cpp" line="23"/>
         <source>Sélectionnez l&apos;image</source>
         <translation type="unfinished">Select image</translation>
     </message>
     <message>
-        <location filename="mainwindowmenu.cpp" line="20"/>
+        <location filename="mainwindowmenu.cpp" line="24"/>
+        <location filename="mainwindowmenu.cpp" line="45"/>
         <source>Fichier Image (*.png *.jpg *.bmp)</source>
         <translation type="unfinished">Image File</translation>
+    </message>
+    <message>
+        <location filename="mainwindowmenu.cpp" line="44"/>
+        <source>Sauvegarder l&apos;image</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
