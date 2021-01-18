4ba381867aab0b6b325b0f2d20907e4ab1521d9d
Anthony <anthony_pro1998@outlook.fr>
2021-01-18 10:57:14 +0100

    fix trim when not region selectionned

--- mainwindowmenu.cpp
+++ mainwindowmenu.cpp
@@ -15,6 +15,7 @@ mainWindowMenu::mainWindowMenu(QWidget *parent)
     labelForImage->setVisible(false);
     scrollAreaForImage->setVisible(false);
     runAllEventFromTheMainWindow();
+    actionRogner->setEnabled(false);
 }
 
 void mainWindowMenu::openNewFile(){
@@ -65,39 +66,42 @@ void mainWindowMenu::doResizing(QImage img, int x,int y){
 }
 
 void mainWindowMenu::doTrim(QImage img, int trimSelect){
-    if(trimSelect== 1){
-        theImg = img.copy(formAndCrop->x,formAndCrop->y,formAndCrop->lastP,formAndCrop->firstP );
-        refreshImage();
+    if(formAndCrop!=nullptr and formAndCrop->x != formAndCrop->y){
+        if(trimSelect== 1){
+            theImg = img.copy(formAndCrop->x,formAndCrop->y,formAndCrop->lastP,formAndCrop->firstP );
+            refreshImage();
+            scrollAreaForImage->move(formAndCrop->x, formAndCrop->y);
+        }
+        else if(trimSelect == 2){
+
+            QPixmap target = QPixmap(size());
+            target.fill(Qt::transparent);
+
+            QPixmap p = QPixmap::fromImage(img);
+            //p.scaled(200, 200, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+
+
+            QPainter painter (&target);
+
+            painter.setRenderHint(QPainter::Antialiasing, true);
+            painter.setRenderHint(QPainter::HighQualityAntialiasing, true);
+            painter.setRenderHint(QPainter::SmoothPixmapTransform, true);
+
+            QPainterPath path = QPainterPath();
+           // path.addRoundedRect(formAndCrop->x,formAndCrop->y, formAndCrop->lastP,formAndCrop->firstP, 50, 50);
+            path.addEllipse(formAndCrop->x,formAndCrop->y, formAndCrop->lastP,formAndCrop->firstP);
+            std::cout<<formAndCrop->x<<" "<<formAndCrop->y;
+            std::cout<<"\n";
+            std::cout<<formAndCrop->lastP<<" "<<formAndCrop->firstP;
+            painter.setClipPath(path);
+            painter.drawPixmap(0, 0, p);
+            labelForImage->setPixmap(target);
+
+        }
+
+        formAndCrop->clearImage();
+       // scrollAreaForImage->move(formAndCrop->x, formAndCrop->y);
     }
-    else if(trimSelect == 2){
-
-        QPixmap target = QPixmap(size());
-        target.fill(Qt::transparent);
-
-        QPixmap p = QPixmap::fromImage(img);
-        //p.scaled(200, 200, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
-
-
-        QPainter painter (&target);
-
-        painter.setRenderHint(QPainter::Antialiasing, true);
-        painter.setRenderHint(QPainter::HighQualityAntialiasing, true);
-        painter.setRenderHint(QPainter::SmoothPixmapTransform, true);
-
-        QPainterPath path = QPainterPath();
-       // path.addRoundedRect(formAndCrop->x,formAndCrop->y, formAndCrop->lastP,formAndCrop->firstP, 50, 50);
-        path.addEllipse(formAndCrop->x,formAndCrop->y, formAndCrop->lastP,formAndCrop->firstP);
-        std::cout<<formAndCrop->x<<" "<<formAndCrop->y;
-        std::cout<<"\n";
-        std::cout<<formAndCrop->lastP<<" "<<formAndCrop->firstP;
-        painter.setClipPath(path);
-        painter.drawPixmap(0, 0, p);
-        labelForImage->setPixmap(target);
-
-    }
-
-    formAndCrop->clearImage();
-   // scrollAreaForImage->move(formAndCrop->x, formAndCrop->y);
 
 
 }
@@ -107,12 +111,14 @@ void mainWindowMenu::selectMode(QImage img, int trimSelect){
         this->layout()->removeWidget(formAndCrop);
         formAndCrop->clearImage();
     }
+
     modState = trimSelect;
     formAndCrop = new FormsAndCrop(scrollAreaForImage->x(), scrollAreaForImage->y(),
                                          scrollAreaForImage->height()-scrollAreaForImage->horizontalScrollBar()->height(),
                                           scrollAreaForImage->width()-scrollAreaForImage->verticalScrollBar()->width(),trimSelect);
     mode = 3;
     this->layout()->addWidget(formAndCrop);
+    actionRogner->setEnabled(true);
 
 
 }
