0427ec54bd3005a6d5b2d7ce3ee2714a253dd672
sykom1 <saber.tnifas@etu.univ-amu.fr>
2021-01-23 11:38:51 +0100

    premiere tentative d'automatisation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <QApplication>
 #include <QTranslator>
 #include <QInputDialog>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 
 
 int main(int argc, char *argv[])
@@ -12,21 +17,50 @@ int main(int argc, char *argv[])
     QTranslator t;
 
     QStringList languages;
-    languages << "Français" << "English";
+    languages << "Francais" << "English";
+    QString lang;
+    fstream myfile;
+    myfile.open ("options.txt");
+    string line;
 
-    QString lang = QInputDialog::getItem(NULL,"Selectionnez La Langue",
-                                                        "Langage",languages);
+    if( myfile.tellg() == 0 ){
+
+        lang = QInputDialog::getItem(NULL,"Selectionnez La Langue",
+                                                           "Langage",languages);
+        std::cout << "on passe ici" << std::endl;
+        std::cout << lang.toStdString() << std::endl;
+
+        myfile << lang.toStdString();
+        myfile.close();
+    }else{
+          if (myfile.is_open())
+          {
+              std::cout << "On passe plutot ici" << std::endl;
+              std::cout << lang.toStdString();
+            while (getline(myfile,line) )
+            {
+              lang = QString::fromStdString(lang.toStdString());
+            }
+            myfile.close();
+          }
+
+          else cout << "Unable to open file";
+      }
+
 
     if( lang == "English"){
         t.load(":/english.qm");
 
     }
 
-    else if( lang != "Français"){
+    else if( lang != "Francais"){
 
         printf("Cette langue n'existe pas \n");
         return 0;
     }
+
+
+
      a.installTranslator(&t);
 
     mainWindowMenu *w = new mainWindowMenu(&t);
